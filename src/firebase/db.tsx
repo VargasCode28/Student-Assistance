d99539e7d9dd477fb5e9a22db3251a43484c5c3f
leonardo Vargas <lv9334889@gmail.com>
2025-08-20 14:47:19 -0400

    .env security enhancement.

--- src/firebase/db.tsx
+++ src/firebase/db.tsx
@@ -1,24 +1,50 @@
 
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// import { initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth";
+// import { getFirestore } from "firebase/firestore";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCNc7L5sZCQfWrIVshg6u1sd7rQW3wKPPY",
+//   authDomain: "student-assistance-52d14.firebaseapp.com",
+//   projectId: "student-assistance-52d14",
+//   storageBucket: "student-assistance-52d14.firebasestorage.app",
+//   messagingSenderId: "693492777546",
+//   appId: "1:693492777546:web:d77c6425bfbe2edb45dcb3"
+// };
+
+
+// const app = initializeApp(firebaseConfig);
+// const auth = getAuth(app);
+
+
+// export const db = getFirestore(app);
+// export {app, auth};
+
+
+
+
+
+ import { initializeApp } from "firebase/app";
+ import { getAuth } from "firebase/auth";
+ import { getFirestore } from "firebase/firestore";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCNc7L5sZCQfWrIVshg6u1sd7rQW3wKPPY",
-  authDomain: "student-assistance-52d14.firebaseapp.com",
-  projectId: "student-assistance-52d14",
-  storageBucket: "student-assistance-52d14.firebasestorage.app",
-  messagingSenderId: "693492777546",
-  appId: "1:693492777546:web:d77c6425bfbe2edb45dcb3"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+
+ const app = initializeApp(firebaseConfig);
+ const auth = getAuth(app);
 
 
-export const db = getFirestore(app);
-export {app, auth};
+ export const db = getFirestore(app);
+ export {app, auth};
